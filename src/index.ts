158ecd315958a29deff5ea246c169edddd6a92ac
Urmi Rahman <urmi.rahman690@gmail.com>
2025-05-08 20:46:00 +0600

    👍 : problem 2 & 3 done

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
 function formatString(input: string, toUpper?: boolean): string {
   return toUpper ? input.toUpperCase() : input.toLowerCase();
 }
+
+interface iRating {
+  title: string;
+  rating: number;
+}
+
+function filterByRating(items: iRating[]): iRating[] {
+  return items.filter((item) => item.rating >= 4);
+}
+
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+  return arrays.flat();
+}
